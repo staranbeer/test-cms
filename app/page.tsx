6dc206269c1b529c5add9b8e0573b899fab8ab9c
taranbeer <staranbeer@gmail.com>
2022-12-06 12:31:34 +0530

    minor change

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
 import Link from "next/link";
-import { env } from "process";
 
 const Page = () => {
   return (
-    <div>
-      <div>
-        <h1>Authentication yet to be implemented</h1>
-        <Link href="/content/collections">
-          Continue to your saved collections
-        </Link>
-      </div>
-    </div>
+    <html>
+      <head>this is the head</head>
+      <body>
+        <div className="h-screen overflow-hidden grid place-items-center">
+          <div className="flex flex-col gap-4 text-center">
+            <h1>Authentication yet to be implemented</h1>
+            <Link href="/content/collections">
+              Continue to your saved collections
+            </Link>
+          </div>
+        </div>
+      </body>
+    </html>
   );
 };
 
